b3ebbe4ebb492cca9d889e4668e02b67c309e86f
ZIya <ziya@My-World.(none)>
2011-10-13 12:16:24 +0530

    Big bazar first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigBazar::Application.config.secret_token = '9cc91e5ba6a0b4744cffaefc4c3e7fa79bfa558a2d2acb73a13f6e1e0086e904b1b4a5e542fb0665b8c206dd4f91d6b7bf095e4fecb33952a2357b68cbc81984'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BigBazar::Application.config.session_store :cookie_store, key: '_big_bazar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BigBazar::Application.config.session_store :active_record_store
